cd05f541ff5d760b8e0c92ecbf34ea022c18367c
Tr-blank <blank0129@gmail.com>
2018-06-10 23:30:27 +0800

    路徑修正

--- docs/precache-manifest.dc70ae9cc54ce0298780947a184742e5.js
+++ docs/precache-manifest.dc70ae9cc54ce0298780947a184742e5.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
     "revision": "b0c38025056d2a7ecc1d",
-    "url": "/js/chunk-vendors.b0c38025.js"
+    "url": "js/chunk-vendors.b0c38025.js"
   },
   {
     "revision": "d963f330a4c9c9fbbc2e",
-    "url": "/js/app.d963f330.js"
+    "url": "js/app.d963f330.js"
   },
   {
     "revision": "01d55f12cc3f629693a21bc1a43253f7",
@@ -13,6 +13,6 @@ self.__precacheManifest = [
   },
   {
     "revision": "d963f330a4c9c9fbbc2e",
-    "url": "/css/app.0fcd01cf.css"
+    "url": "css/app.0fcd01cf.css"
   }
 ];
